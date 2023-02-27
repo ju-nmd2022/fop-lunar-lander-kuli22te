f683a070841fbc6e62d6805d572e5dd82e4a55cd
kuli22te <kuli22te@student.ju.se>
2023-02-27 17:03:26 +0100

    background and ground

--- lunar-landing.js
+++ lunar-landing.js
@@ -1,1 +1,34 @@
 background(4, 22, 44);
+
+let starA = [];
+let starB = [];
+
+for (let i = 0; i < 1250; i++) {
+  const a = Math.floor(Math.random() * width);
+  const b = Math.floor(Math.random() * height);
+
+  starA.push(a);
+  starB.push(b);
+}
+
+// stars
+
+function stars() {
+  noStroke();
+  fill(225);
+  for (let index in starA) {
+    ellipse(starA[index], starB[index], 3);
+  }
+}
+
+stars();
+
+// ground
+
+function ground() {
+  noStroke();
+  fill(101, 94, 94);
+  rect(0, 440, 1200, 140);
+}
+
+ground();

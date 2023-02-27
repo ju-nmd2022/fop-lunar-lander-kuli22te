408ad2eb773ecb09c8a680d032f8c7f9619b5950
kuli22te <kuli22te@student.ju.se>
2023-02-27 14:53:33 +0100

    27/2

--- try.js
+++ try.js
@@ -1,10 +1,44 @@
-//Looks of the lander
-function ship(shipX, shipY, rotation) {
-  push();
-  translate(shipX, shipY);
-  rotate(rotation);
-  fill(51, 51, 51);
-  stroke(51, 51, 51);
-  ellipse(x, y, x - 150, y - 170);
-  pop();
+background(14, 42, 84);
+
+let starA = [];
+let starB = [];
+
+for (let i = 0; i < 1250; i++) {
+  const a = Math.floor(Math.random() * width);
+  const b = Math.floor(Math.random() * height);
+
+  starA.push(a);
+  starB.push(b);
 }
+
+// stars
+
+function stars() {
+  noStroke();
+  fill(255);
+  for (let index in starA) {
+    ellipse(starA[index], starB[index], 3);
+  }
+}
+
+stars();
+
+//ground
+
+function ground() {
+  fill(101, 94, 94);
+  stroke(101, 94, 94);
+  rect(0, 340, 1200, 240);
+}
+
+ground();
+
+//Ship
+
+push();
+ellipse(-25, -30, 50, 80);
+fill(225);
+rect(-39, -60 + 10, 30, 10, 5);
+pop();
+
+// start pad
